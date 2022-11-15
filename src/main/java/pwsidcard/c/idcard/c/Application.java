9e5d4deedbc2cbcb5a7da4a4f0b4d13f60c77b00
rezaramadhani <rezarmd17@gmail.com>
2022-11-15 19:51:01 +0700

    membuat index

---
+++ src/main/java/pwsidcard/c/idcard/c/Application.java
@@ -0,0 +1,13 @@
+package pwsidcard.c.idcard.c;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/pwsidcard/c/idcard/c/ApplicationTests.java
@@ -0,0 +1,13 @@
+package pwsidcard.c.idcard.c;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
