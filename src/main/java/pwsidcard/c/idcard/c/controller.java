ee603957056367de7fbb8b5cf728328382097329
rezaramadhani <rezarmd17@gmail.com>
2022-11-15 20:11:46 +0700

    mengisi file controller

--- src/main/java/pwsidcard/c/idcard/c/controller.java
+++ src/main/java/pwsidcard/c/idcard/c/controller.java
@@ -5,10 +5,43 @@
  */
 package pwsidcard.c.idcard.c;
 
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import org.apache.tomcat.util.codec.binary.Base64;
+import org.apache.tomcat.util.json.ParseException;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
 /**
  *
  * @author Reza Ramadhani
  */
+@Controller
 public class controller {
-    
+    @RequestMapping("/getData")
+    public String getData(@RequestParam("namalengkap")String text,
+                          @RequestParam("tanggallahir")@DateTimeFormat(pattern="yyyy-MM-dd")Date date,
+                          @RequestParam("foto")MultipartFile file, Model model)throws IOException, ParseException{
+        
+        SimpleDateFormat tanggal = new SimpleDateFormat("EE/dd/MMMM/yyyy");
+        
+        String nTgl = tanggal.format(date);
+        
+        String blob = Base64.encodeBase64String(file.getBytes());
+        String photo = "data:image/jpeg;base64,".concat(blob);
+        
+        model.addAttribute("namalengkap", text);
+        model.addAttribute("tanggallahir", nTgl);
+        model.addAttribute("foto", photo); 
+        
+        
+        return "output";
+        
+    }
+
 }
